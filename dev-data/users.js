2963f2e4028865daf2f5e3bfc7a30cdb0752269d
Shm <malekishayan79@gmail.com>
2021-08-04 22:01:05 +0430

    add some packages

---
+++ dev-data/users.js
@@ -0,0 +1,46 @@
+const users = [
+  {
+    name: 'Ali',
+    email: 'ali312@gmail.com',
+    password: 'test1234',
+    passwordConfirm: 'test1234'
+  },
+  {
+    name: 'Mohammad',
+    email: 'mohama123@dgmail.com',
+    password: 'test1234',
+    passwordConfirm: 'test1234'
+  },
+  {
+    name: 'Reza',
+    email: 'rreza22@gmail.com',
+    password: 'test1234',
+    passwordConfirm: 'test1234'
+  },
+  {
+    name: 'Gholam',
+    email: 'gholammgc21@gmail.com',
+    password: 'test1234',
+    passwordConfirm: 'test1234'
+  },
+  {
+    name: 'mehdi',
+    email: 'mehdi99@gmail.com',
+    password: 'test1234',
+    passwordConfirm: 'test1234'
+  },
+  {
+    name: 'amir',
+    email: 'ammirr241@gmail.com',
+    password: 'test1234',
+    passwordConfirm: 'test1234'
+  },
+  {
+    name: 'Ahmad',
+    email: 'Ahmad11@gmail.com',
+    password: 'test1234',
+    passwordConfirm: 'test1234'
+  }
+];
+
+module.exports = users;
