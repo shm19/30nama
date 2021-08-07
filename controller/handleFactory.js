8e575d3b523842327cb25c234550003683e09f90
Shm <malekishayan79@gmail.com>
2021-08-07 14:47:21 +0430

    added filter,sort,limit Fileds,paginate Features

--- controller/handleFactory.js
+++ controller/handleFactory.js
@@ -1,3 +1,4 @@
+const ApiFeatures = require('../utils/apiFeatures');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 
@@ -32,7 +33,6 @@ module.exports.updateOne = Model =>
 module.exports.deleteOne = Model =>
   catchAsync(async (req, res, next) => {
     const doc = await Model.findByIdAndDelete(req.params.id);
-
     if (!doc) {
       return next(new AppError('No document found with that ID', 404));
     }
@@ -57,14 +57,16 @@ module.exports.createOne = Model =>
 // Filter , Sort , paginate , limit Fields
 module.exports.getAll = Model =>
   catchAsync(async (req, res, next) => {
-    const docs = await Model.find();
-    if (docs.length > 0) {
-      res.status(200).json({
-        status: 'success',
-        results: docs.length,
-        data: {
-          data: docs
-        }
-      });
-    }
+    const docs = await new ApiFeatures(Model.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate().query;
+    res.status(200).json({
+      status: 'success',
+      results: docs.length,
+      data: {
+        data: docs
+      }
+    });
   });
--- utils/apiFeatures.js
+++ utils/apiFeatures.js
@@ -1,16 +1,59 @@
 class ApiFeatures {
   constructor(query, queryStr) {
-    this.query = query;
-    this.queryStr = queryStr;
+    this._query = query;
+    this._queryStr = queryStr;
   }
 
-  filter() {}
+  filter() {
+    const excludedFields = ['limit', 'sort', 'fields', 'page'];
+    // remvoe fields from query str []
+    let filterQuery = { ...this._queryStr };
+    excludedFields.forEach(el => delete filterQuery[el]);
+    filterQuery = JSON.parse(
+      JSON.stringify(filterQuery).replace(
+        /\b(gte|lte|lt|gt)\b/g,
+        match => `$${match}`
+      )
+    );
+    this._query = this._query.find(filterQuery);
+    return this;
+  }
 
-  sort() {}
+  sort() {
+    if (this._queryStr.sort) {
+      const sortBy = this._queryStr.sort.split(',').join(' ');
+      this._query = this._query.sort(sortBy);
+    } else {
+      // fefault sort ==> you can also add a createAt field and use that to sort with time
+      this._query.sort('name');
+    }
+    return this;
+  }
 
-  limit() {}
+  limitFields() {
+    if (this._queryStr.fields) {
+      const selectedFields = this._queryStr.fields.split(',').join(' ');
+      this._query.select(selectedFields);
+    } else {
+      this._query.select('-__v');
+    }
+    return this;
+  }
 
-  paginate() {}
+  paginate() {
+    const limit = this._queryStr.limit * 1;
+    const page = this._queryStr.page * 1;
+    if (page && limit) {
+      this._query = this._query.skip((page - 1) * limit).limit(limit);
+    } else {
+      this._query = this._query.skip(0).limit(10);
+    }
+    return this;
+  }
+
+  get query() {
+    return this._query;
+  }
 }
 
 module.exports = ApiFeatures;
